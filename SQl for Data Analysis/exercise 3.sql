23656f93a6418fda00b21b2056d38416928fa85d
Roxana <60154537+roxsara@users.noreply.github.com>
2022-06-29 09:59:06 +0300

    Delete SQl for Data Analysis directory

--- SQl for Data Analysis/exercise 3.sql
+++
@@ -1,9 +0,0 @@
---Exercise 3:
-
---Select the first 100 rows from the Sales.SalesPerson table.
---Display only the SalesLastYear and SalesYTD columns, in that order.
-
-SELECT TOP (100)
-      [SalesLastYear]
-      ,[SalesYTD]
-  FROM [AdventureWorks2019].[Sales].[SalesPerson]
